00321f380815fada1782a0cd8d2d69e575efd144
fengyifan <1106132246@qq.com>
2018-03-02 20:18:42 +0800

    modify train helper

---
+++ train_helper.py
@@ -0,0 +1,8 @@
+import torch
+
+def get_model(cfg, dataset='modelnet40', resume=True):
+    pass
+
+
+def save_model_and_optim(cfg, model, optimizer, epoch, best_prec1, best_confusion_mat):
+    pass
